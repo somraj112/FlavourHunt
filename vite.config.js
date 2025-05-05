e592e1f86d872ffa42cf5940625b6e78e75b08e1
somraj112 <somrajnandi112@gmail.com>
2025-05-05 22:36:49 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FlavourHunt/',
   plugins: [react(),
      tailwindcss()
-    ],
-  base: '/FlavourHunt/',
+    ]
 })
